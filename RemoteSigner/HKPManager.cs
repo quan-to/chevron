1aeb6a85eec0fbb30408dc3f4dea94f90aea0c10
Lucas Teske <lucas@teske.net.br>
2018-05-17 06:57:14 -0300

    Added PKS Operation GET for key lookup / search.

--- RemoteSigner/HKPManager.cs
+++ RemoteSigner/HKPManager.cs
@@ -13,18 +13,23 @@ namespace RemoteSigner {
         public string Lookup(string operation, string options, string machineReadable, string noModification, string showFingerprint, string exactMatch, string searchData) {
             switch (operation) {
                 case Operation.Get:
-                    return OperationGet(options, machineReadable.ToLower() == "on", noModification.ToLower() == "on", searchData);
+                    return OperationGet(options, machineReadable != null && machineReadable.ToLower() == "on", noModification != null && noModification.ToLower() == "on", searchData);
                 case Operation.Index:
-                    return OperationIndex(options, machineReadable.ToLower() == "on", noModification.ToLower() == "on", showFingerprint.ToLower() == "on", exactMatch.ToLower() == "on", searchData);
+                    return OperationIndex(options, machineReadable != null && machineReadable.ToLower() == "on", noModification != null && noModification.ToLower() == "on", showFingerprint != null && showFingerprint.ToLower() == "on", exactMatch != null && exactMatch.ToLower() == "on", searchData);
                 case Operation.Vindex:
-                    return OperationVIndex(options, machineReadable.ToLower() == "on", noModification.ToLower() == "on", showFingerprint.ToLower() == "on", exactMatch.ToLower() == "on", searchData);
+                    return OperationVIndex(options, machineReadable != null && machineReadable.ToLower() == "on", noModification != null && noModification.ToLower() == "on", showFingerprint != null && showFingerprint.ToLower() == "on", exactMatch != null && exactMatch.ToLower() == "on", searchData);
                 default:
                     throw new UnknownOperationException(operation);
             }
         }
 
         string OperationGet(string options, bool machineReadable, bool noModification, string searchData) {
-            throw new OperationNotImplemented("index");
+            if (searchData.StartsWith("0x", StringComparison.InvariantCulture)) {
+                return pks.GetKey(searchData.Substring(2));
+            }
+
+            var results = pks.Search(searchData, 0, 1);
+            return results.Count > 0 ? results[0].AsciiArmoredPublicKey : null;
         }
 
         string OperationIndex(string options, bool machineReadable, bool noModification, bool showFingerPrint, bool exactMatch, string searchData) {
