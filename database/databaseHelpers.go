1ca3e640090688d8587b62d05c98b7b1e98214ed
Lucas Teske <lucas@teske.net.br>
2019-02-26 03:05:15 -0300

    Reorganized test tools

--- database/databaseHelpers.go
+++ database/databaseHelpers.go
@@ -1,9 +1,7 @@
 package database
 
 import (
-	"github.com/quan-to/remote-signer"
 	r "gopkg.in/rethinkdb/rethinkdb-go.v5"
-	"time"
 )
 
 func GetTableIndexes(tableName string) []string {
@@ -79,90 +77,4 @@ func GetTables() []string {
 	}
 
 	return tbs
-}
-
-func NumNodes() int {
-	var conn = GetConnection()
-	c, err := r.DB("rethinkdb").
-		Table("stats").
-		Filter(
-			r.Row.HasFields("server").
-				And(
-					r.Row.HasFields("table").Not(),
-				)).
-		Count().
-		Run(conn)
-	if err != nil {
-		dbLog.Error("Error fetching number of nodes: %v", err)
-		return 1
-	}
-
-	z, err := c.Interface()
-
-	if err != nil {
-		dbLog.Error("Error fetching number of nodes: %v", err)
-		return 1
-	}
-
-	count := int(z.(float64))
-
-	return count
-}
-
-func WaitDatabaseDrop(database string) {
-	dbLog.Info("Waiting for database drop")
-	nodes := NumNodes() * 4
-	for i := 0; i < nodes; i++ {
-		dbs := GetDatabases()
-		for remote_signer.StringIndexOf(database, dbs) > -1 {
-			time.Sleep(1 * time.Second)
-			dbs = GetDatabases()
-		}
-		time.Sleep(1 * time.Second)
-	}
-}
-
-func WaitDatabaseCreate(database string) {
-	dbLog.Info("Waiting for database create")
-	nodes := NumNodes() * 4
-	for i := 0; i < nodes; i++ {
-		dbs := GetDatabases()
-		for remote_signer.StringIndexOf(database, dbs) == -1 {
-			time.Sleep(1 * time.Second)
-			dbs = GetDatabases()
-		}
-		time.Sleep(1 * time.Second)
-	}
-	_ = r.DB(database).
-		Wait(r.WaitOpts{Timeout: 0}).
-		Exec(GetConnection())
-}
-
-func WaitTableCreate(table string) {
-	dbLog.Info("Waiting for table create")
-	nodes := NumNodes() * 4
-	for i := 0; i < nodes; i++ {
-		tables := GetTables()
-		for remote_signer.StringIndexOf(table, tables) == -1 {
-			time.Sleep(1 * time.Second)
-			tables = GetTables()
-		}
-		time.Sleep(100 * time.Millisecond)
-	}
-	_ = r.DB(remote_signer.DatabaseName).
-		Table(table).
-		Wait(r.WaitOpts{Timeout: 0}).
-		Exec(GetConnection())
-}
-
-func WaitTableIndexCreate(table, index string) {
-	dbLog.Info("Waiting for index create")
-	nodes := NumNodes() * 4
-	for i := 0; i < nodes; i++ {
-		indexes := GetTableIndexes(table)
-		for remote_signer.StringIndexOf(index, indexes) == -1 {
-			time.Sleep(100 * time.Millisecond)
-			indexes = GetTableIndexes(table)
-		}
-	}
 }
--- tools.go
+++ tools.go
@@ -17,13 +17,10 @@ import (
 	"io"
 	"io/ioutil"
 	"math/rand"
-	"net"
 	"os"
-	"os/exec"
 	"path"
 	"regexp"
 	"strings"
-	"time"
 )
 
 var toolsLog = SLog.Scope("Tools")
@@ -526,79 +523,4 @@ func GeneratePassword() string {
 		b[i] = passwordBytes[rand.Int63()%int64(len(passwordBytes))]
 	}
 	return string(b)
-}
-
-// RQLStart please don't judge me. That its what it takes to avoid RethinkDB Non-Atomic Database operations on tests :(
-func RQLStart() (*exec.Cmd, error) {
-	genPort := 28020 + rand.Intn(100)
-	_ = os.RemoveAll("./rethinkdb_data")
-	connString := fmt.Sprintf("127.0.0.1:%d", genPort)
-	conn, err := net.DialTimeout("tcp", connString, time.Millisecond*500)
-	if err == nil {
-		// Retry another port
-		_ = conn.Close()
-		return RQLStart()
-	}
-
-	SLog.Info("Starting RethinkDB")
-	cmd := exec.Command("rethinkdb", "--no-http-admin", "--bind", "127.0.0.1", "--cluster-port", fmt.Sprintf("%d", genPort+200), "--driver-port", fmt.Sprintf("%d", genPort))
-	//cmd.Stdout = os.Stdout
-	//cmd.Stderr = os.Stderr
-	err = cmd.Start()
-
-	if err != nil {
-		b, _ := cmd.Output()
-		SLog.Error(string(b))
-		panic(fmt.Errorf("cannot start rethinkdb: %s", err))
-	}
-
-	RethinkDBPort = genPort
-
-	SLog.Info("Waiting for rethink to settle")
-	time.Sleep(time.Second)
-	retry := 0
-	started := false
-
-	for retry < 5 {
-		conn, err := net.DialTimeout("tcp", connString, time.Millisecond*500)
-		if err != nil {
-			SLog.Error(err)
-			retry++
-			time.Sleep(time.Second)
-			continue
-		}
-		_ = conn.Close()
-		started = true
-		break
-	}
-
-	if !started {
-		return nil, fmt.Errorf("timeout waiting for rethinkdb to start")
-	}
-
-	return cmd, nil
-}
-
-func RQLStop(cmd *exec.Cmd) {
-	SLog.Info("Stopping RethinkDB")
-	err := cmd.Process.Signal(os.Kill)
-	if err != nil {
-		panic(fmt.Errorf("error killing rethinkdb: %s", err))
-	}
-	err = cmd.Process.Kill()
-	if err != nil {
-		panic(fmt.Errorf("error killing rethinkdb: %s", err))
-	}
-
-	err = os.RemoveAll("./rethinkdb_data")
-	if err != nil {
-		panic(fmt.Errorf("error erasing rethinkdb folder: %s", err))
-	}
-
-	_, err = cmd.Process.Wait()
-	if err != nil {
-		panic(fmt.Errorf("error waiting rethinkdb: %s", err))
-	}
-
-	time.Sleep(10 * time.Second)
 }
