8ace1ac2876d81ed2e085f0af2176f4978d6679a
Lucas Teske <lucas@teske.net.br>
2019-03-30 16:18:06 -0300

    Changed bundler to work on travis

--- cmd/agent-ui/bundleit.sh
+++ cmd/agent-ui/bundleit.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
+cat << EOF > bind.go
+// +build !linux,amd64
+// +build !darwin,amd64
+// +build !windows,amd64
+
+// Placeholder file for assets binding
+package main
+
+func Asset(name string) ([]byte, error) {
+	return []byte{}, nil
+}
+
+func AssetDir(name string) ([]string, error) {
+	return []string{}, nil
+}
+
+func RestoreAssets(dir, name string) error {
+	return nil
+}
+EOF
+
 astilectron-bundler -logger-app-name=AgentUI -d -w -l
