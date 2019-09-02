0dadecfc24613967f459b6333eacdb446f35bd80
Emanuel Júnior <emanuelmpjr@gmail.com>
2019-09-02 13:35:40 -0300

    Check external dependencies

--- server/tests.go
+++ server/tests.go
@@ -2,7 +2,10 @@ package server
 
 import (
 	"github.com/gorilla/mux"
+	remote_signer "github.com/quan-to/chevron"
 	"github.com/quan-to/chevron/models"
+	"github.com/quan-to/chevron/database"
+	"github.com/quan-to/chevron/vaultManager"
 	"net/http"
 )
 
@@ -16,9 +19,44 @@ func (ge *TestsEndpoint) AttachHandlers(r *mux.Router) {
 	r.HandleFunc("/ping", ge.ping)
 }
 
+func (ge *TestsEndpoint) checkExternal() bool {
+	isHealthy := true
+
+	if remote_signer.EnableRethinkSKS {
+		conn := database.GetConnection()
+
+		if !conn.IsConnected() {
+			isHealthy = false
+		}
+	}
+
+	if remote_signer.VaultStorage {
+		kb := vaultManager.MakeVaultManager(remote_signer.KeyPrefix)
+		health, err := kb.Client.Sys().Health()
+
+		if err != nil {
+			isHealthy = false
+		}
+
+		if !health.Initialized || health.Sealed {
+			isHealthy = false
+		}
+	}
+
+	return isHealthy
+}
+
 func (ge *TestsEndpoint) ping(w http.ResponseWriter, r *http.Request) {
+	isHealthy := ge.checkExternal()
+
 	// Do not log here. This call will flood the log
 	w.Header().Set("Content-Type", models.MimeText)
-	w.WriteHeader(200)
-	_, _ = w.Write([]byte("OK"))
+
+	if isHealthy {
+		w.WriteHeader(200)
+		_, _ = w.Write([]byte("OK"))
+	} else {
+		w.WriteHeader(503)
+		_, _ = w.Write([]byte("Service Unavailable"))
+	}
 }
