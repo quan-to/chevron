f5aade987ee633f70a7209c6d74c93ddaf7f8b63
Emanuel Junior <emanuelmpjr@gmail.com>
2019-10-02 10:34:49 -0300

    Add context in internal endpoint

--- server/internalEndpoint_test.go
+++ server/internalEndpoint_test.go
@@ -2,14 +2,16 @@ package server
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/quan-to/chevron"
-	"github.com/quan-to/chevron/QuantoError"
-	"github.com/quan-to/chevron/rstest"
 	"io/ioutil"
 	"net/http"
 	"testing"
+
+	remote_signer "github.com/quan-to/chevron"
+	"github.com/quan-to/chevron/QuantoError"
+	"github.com/quan-to/chevron/rstest"
 )
 
 /*
@@ -44,16 +46,17 @@ func TestUnlockLocalKeys(t *testing.T) {
 */
 
 func TestGetUnlockPasswords(t *testing.T) {
+	ctx := context.Background()
 	filename := fmt.Sprintf("key-password-utf8-%s.txt", rstest.TestKeyFingerprint)
 
-	encPass, err := gpg.Encrypt(filename, sm.GetMasterKeyFingerPrint(), []byte(rstest.TestKeyPassword), remote_signer.SMEncryptedDataOnly)
+	encPass, err := gpg.Encrypt(ctx, filename, sm.GetMasterKeyFingerPrint(ctx), []byte(rstest.TestKeyPassword), remote_signer.SMEncryptedDataOnly)
 
 	if err != nil {
 		t.Errorf("Error saving password: %s", err)
 		t.FailNow()
 	}
 
-	sm.PutEncryptedPassword(rstest.TestKeyFingerprint, encPass)
+	sm.PutEncryptedPassword(ctx, rstest.TestKeyFingerprint, encPass)
 
 	req, err := http.NewRequest("GET", "/__internal/__getUnlockPasswords", nil)
 
@@ -96,9 +99,10 @@ func TestGetUnlockPasswords(t *testing.T) {
 }
 
 func TestPostUnlockPassword(t *testing.T) {
+	ctx := context.Background()
 	filename := fmt.Sprintf("key-password-utf8-%s.txt", rstest.TestKeyFingerprint)
 
-	encPass, err := gpg.Encrypt(filename, sm.GetMasterKeyFingerPrint(), []byte(rstest.TestKeyFingerprint), remote_signer.SMEncryptedDataOnly)
+	encPass, err := gpg.Encrypt(ctx, filename, sm.GetMasterKeyFingerPrint(ctx), []byte(rstest.TestKeyFingerprint), remote_signer.SMEncryptedDataOnly)
 
 	if err != nil {
 		t.Errorf("Error saving password: %s", err)
@@ -134,7 +138,7 @@ func TestPostUnlockPassword(t *testing.T) {
 		t.Errorf("Expected OK got %s", string(d))
 	}
 
-	passwords := sm.GetPasswords()
+	passwords := sm.GetPasswords(ctx)
 	if passwords[rstest.TestKeyFingerprint] != encPass {
 		t.Errorf("Expected key %s to be in the password list.", rstest.TestKeyFingerprint)
 	}
