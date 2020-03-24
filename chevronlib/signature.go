9079d2103f302c97c2d4b6bab5989cf5f0da0f4f
Lucas Teske <lucas@contaquanto.com.br>
2020-03-24 17:28:02 -0300

    feat(chevronlib): Add Quanto Sign/Verify Methods to ChevronLIB

--- chevronlib/signature.go
+++ chevronlib/signature.go
@@ -3,6 +3,7 @@ package chevronlib
 import (
 	"crypto"
 	"encoding/base64"
+	remote_signer "github.com/quan-to/chevron"
 )
 
 // LoadKey loads a private or public key into the memory keyring
@@ -24,6 +25,27 @@ func VerifySignature(data []byte, signature string) (result bool, err error) {
 	return pgpBackend.VerifySignature(ctx, data, signature)
 }
 
+// QuantoVerifySignature verifies a signature in Quanto Signature Format using a already loaded public key
+// export VerifySignature
+func QuantoVerifySignature(data []byte, signature string) (result bool, err error) {
+	signature = remote_signer.Quanto2GPG(signature)
+
+	return pgpBackend.VerifySignature(ctx, data, signature)
+}
+
+// QuantoVerifyBase64DataSignature verifies a signature using a already loaded public key.
+// The b64data is a raw binary data encoded in base64 string
+// export VerifyBase64DataSignature
+func QuantoVerifyBase64DataSignature(b64data, signature string) (result bool, err error) {
+	var data []byte
+	data, err = base64.StdEncoding.DecodeString(b64data)
+	if err != nil {
+		return
+	}
+
+	return QuantoVerifySignature(data, signature)
+}
+
 // VerifyBase64DataSignature verifies a signature using a already loaded public key. The b64data is a raw binary data encoded in base64 string
 // export VerifyBase64DataSignature
 func VerifyBase64DataSignature(b64data, signature string) (result bool, err error) {
@@ -42,7 +64,19 @@ func SignData(data []byte, fingerprint string) (result string, err error) {
 	return pgpBackend.SignData(ctx, fingerprint, data, crypto.SHA512)
 }
 
-// SignBase64Data signs data using a already loaded and unlocked private key. The b64data is a raw binary data encoded in base64 string
+// QuantoSignData signs the data using a already loaded and unlocked private key and returning in Quanto PGP Signature format
+func QuantoSignData(data []byte, fingerprint string) (result string, err error) {
+	result, err = pgpBackend.SignData(ctx, fingerprint, data, crypto.SHA512)
+	if err != nil {
+		return "", err
+	}
+	result = remote_signer.GPG2Quanto(result, fingerprint, "SHA512")
+
+	return result, nil
+}
+
+// SignBase64Data signs data using a already loaded and unlocked private key.
+// The b64data is a raw binary data encoded in base64 string
 // export SignBase64Data
 func SignBase64Data(b64data, fingerprint string) (result string, err error) {
 	var data []byte
@@ -52,4 +86,16 @@ func SignBase64Data(b64data, fingerprint string) (result string, err error) {
 	}
 
 	return SignData(data, fingerprint)
+}
+
+// QuantoSignData signs the data using a already loaded and unlocked private key and returning in Quanto Signature format.
+//  The b64data is a raw binary data encoded in base64 string
+func QuantoSignBase64Data(b64data, fingerprint string) (result string, err error) {
+	var data []byte
+	data, err = base64.StdEncoding.DecodeString(b64data)
+	if err != nil {
+		return
+	}
+
+	return QuantoSignData(data, fingerprint)
 }
