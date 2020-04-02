e11a5c48338c9728ad82ae0bbfef98672548fa98
Lucas Teske <lucas@contaquanto.com.br>
2020-04-02 13:32:17 -0300

    fix(agentui): Fix log message tag

--- cmd/agent-ui/handleMessages.go
+++ cmd/agent-ui/handleMessages.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/asticode/go-astilectron"
 	"github.com/asticode/go-astilectron-bootstrap"
+	remote_signer "github.com/quan-to/chevron"
 	"github.com/quan-to/slog"
 	"strings"
 )
@@ -22,7 +23,7 @@ const (
 	messageLoadPrivateKeyResult = "loadPrivateKeyResult"
 )
 
-var electronLog = slog.Scope("Electron")
+var electronLog = slog.Scope("Electron").Tag(remote_signer.DefaultTag)
 
 // handleMessages handles messages
 func handleMessages(_ *astilectron.Window, m bootstrap.MessageIn) (payload interface{}, err error) {
