9eb65047e3ce1db2efeec38e28a91a6464387c6c
Lucas Teske <lucas@teske.net.br>
2020-08-17 17:28:13 -0300

    WIP Wasm fixes

--- internal/etc/magicbuilder/pgp_wasm.go
+++ internal/etc/magicbuilder/pgp_wasm.go
@@ -5,16 +5,17 @@ import (
 	"github.com/quan-to/chevron/internal/keybackend"
 	"github.com/quan-to/chevron/internal/keymagic"
 	"github.com/quan-to/chevron/pkg/interfaces"
+	"github.com/quan-to/slog"
 )
 
 // MakePGP creates a new PGPManager using environment variables KeyPrefix, PrivateKeyFolder
 func MakePGP(log slog.Instance) interfaces.PGPManager {
 	kb := keybackend.MakeSaveToDiskBackend(log, config.PrivateKeyFolder, config.KeyPrefix)
 
-	return keymagic.MakePGPManager(log, kb, keymagic.MakeKeyRingManager())
+	return keymagic.MakePGPManager(log, kb, keymagic.MakeKeyRingManager(log))
 }
 
 // MakeVoidPGP creates a PGPManager that does not store anything anywhere
 func MakeVoidPGP(log slog.Instance) interfaces.PGPManager {
-	return keymagic.MakePGPManager(log, keybackend.MakeVoidBackend(), keymagic.MakeKeyRingManager())
+	return keymagic.MakePGPManager(log, keybackend.MakeVoidBackend(), keymagic.MakeKeyRingManager(log))
 }
---
+++ pkg/QuantoError/errorobject_wasm.go
@@ -0,0 +1,63 @@
+package QuantoError
+
+import (
+    "fmt"
+    "github.com/graphql-go/graphql/gqlerrors"
+    "log"
+)
+
+
+func EnableStackTrace() {}
+
+func DisableStackTrace() {}
+
+//Flag to define if a stack trace is returned in response or not
+func ShowStackTrace() bool {
+    return false
+}
+
+type ErrorObject struct {
+    ErrorCode  string      `json:"errorCode"`
+    ErrorField string      `json:"errorField"`
+    Message    string      `json:"message"`
+    ErrorData  interface{} `json:"errorData"`
+    StackTrace string      `json:"stackTrace"`
+}
+
+func New(errorCode, errorField, message string, errorData interface{}) *ErrorObject {
+    return &ErrorObject{
+        ErrorCode:  errorCode,
+        ErrorField: errorField,
+        ErrorData:  errorData,
+        Message:    message,
+    }
+}
+
+func (e *ErrorObject) Error() string {
+    return e.Message
+}
+
+func (e *ErrorObject) ToFormattedError() gqlerrors.FormattedError {
+    log.Println(e)
+    baseErr := gqlerrors.FormatError(e)
+
+    if baseErr.Extensions == nil {
+        baseErr.Extensions = make(map[string]interface{})
+    }
+
+    //baseErr.Extensions["errorObject"] = e
+    baseErr.Extensions["errorCode"] = e.ErrorCode
+    baseErr.Extensions["errorField"] = e.ErrorField
+    baseErr.Extensions["errorData"] = e.ErrorData
+
+    return baseErr
+}
+
+func (e *ErrorObject) String() string {
+    o := fmt.Sprintf("Error: %s\n", e.Message)
+    o += fmt.Sprintf("  Error Code: %s\n", e.ErrorCode)
+    o += fmt.Sprintf("  Error Field: %s\n", e.ErrorField)
+    o += fmt.Sprintf("  Error Data: %v\n", e.ErrorData)
+    o += fmt.Sprintf("  Stack Trace %s\n", e.StackTrace)
+    return o
+}
