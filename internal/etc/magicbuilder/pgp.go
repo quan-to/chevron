e264aeb017f9ef03d48c8b856d3c2cf18ef970da
Lucas Teske <lucas@teske.net.br>
2020-12-09 12:41:02 -0300

    chore(pgp): Change DatabaseHandler to type alias on magicbuilder

--- internal/etc/magicbuilder/pgp.go
+++ internal/etc/magicbuilder/pgp.go
@@ -8,26 +8,10 @@ import (
 	"github.com/quan-to/chevron/internal/keymagic"
 	"github.com/quan-to/chevron/internal/vaultManager"
 	"github.com/quan-to/chevron/pkg/interfaces"
-	"github.com/quan-to/chevron/pkg/models"
 	"github.com/quan-to/slog"
 )
 
-type DatabaseHandler interface {
-	GetUser(username string) (um *models.User, err error)
-	AddUserToken(ut models.UserToken) (string, error)
-	RemoveUserToken(token string) (err error)
-	GetUserToken(token string) (ut *models.UserToken, err error)
-	InvalidateUserTokens() (int, error)
-	AddUser(um models.User) (string, error)
-	UpdateUser(um models.User) error
-	AddGPGKey(key models.GPGKey) (string, bool, error)
-	FindGPGKeyByEmail(email string, pageStart, pageEnd int) ([]models.GPGKey, error)
-	FindGPGKeyByFingerPrint(fingerPrint string, pageStart, pageEnd int) ([]models.GPGKey, error)
-	FindGPGKeyByValue(value string, pageStart, pageEnd int) ([]models.GPGKey, error)
-	FindGPGKeyByName(name string, pageStart, pageEnd int) ([]models.GPGKey, error)
-	FetchGPGKeyByFingerprint(fingerprint string) (*models.GPGKey, error)
-	HealthCheck() error
-}
+type DatabaseHandler keymagic.DatabaseHandler
 
 // MakePGP creates a new PGPManager using environment variables VaultStorage, KeyPrefix, PrivateKeyFolder
 func MakePGP(log slog.Instance, dbHandler DatabaseHandler) interfaces.PGPManager {
