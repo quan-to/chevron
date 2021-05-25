1560db6f245bdf744205e15f9c82c7568946ea8e
Lucas Teske <lucas@teske.net.br>
2021-05-25 15:19:10 -0300

    feat(stream): Add Stream Encrypter to chevronlib

--- pkg/chevronlib/creators.go
+++ pkg/chevronlib/creators.go
@@ -1,9 +1,15 @@
 package chevronlib
 
 import (
+	"crypto"
+	"fmt"
+	"io"
+
 	"github.com/quan-to/chevron/internal/keybackend"
 	"github.com/quan-to/chevron/internal/keymagic"
 	"github.com/quan-to/chevron/pkg/interfaces"
+	"github.com/quan-to/chevron/pkg/openpgp"
+	"github.com/quan-to/chevron/pkg/openpgp/packet"
 	"github.com/quan-to/slog"
 )
 
@@ -24,4 +30,44 @@ func MakeKeyRingManager(log slog.Instance) interfaces.KeyRingManager {
 // log instance can be nil
 func MakePGPManager(log slog.Instance, storage interfaces.StorageBackend, keyRingManager interfaces.KeyRingManager) interfaces.PGPManager {
 	return keymagic.MakePGPManager(log, storage, keyRingManager)
+}
+
+// GetStreamEncrypter returns a PGP Encrypter IO Writer
+func GetStreamEncrypter(signerFingerprint string, encryptToFingerprints []string, output io.Writer, filehint *openpgp.FileHints, config *packet.Config) (enc io.WriteCloser, err error) {
+	var signer *openpgp.Entity
+	var pubkeys []*openpgp.Entity
+
+	if len(signerFingerprint) != 0 {
+		list := GetPrivateKeyEntity(signerFingerprint)
+		for _, k := range list {
+			if k.PrivateKey != nil {
+				signer = k
+				break
+			}
+		}
+		if signer == nil {
+			return nil, fmt.Errorf("cannot find private key with fingerprint %s", signerFingerprint)
+		}
+		if signer.PrivateKey.Encrypted {
+			return nil, fmt.Errorf("found private key %s but it's encrypted", signerFingerprint)
+		}
+	}
+
+	for _, pubKeyFp := range encryptToFingerprints {
+		pub := GetPublicKeyEntity(pubKeyFp)
+		if pub == nil {
+			return nil, fmt.Errorf("cannot find public key with fingerprint %s", pubKeyFp)
+		}
+		pubkeys = append(pubkeys, pub)
+	}
+
+	if config == nil {
+		config = &packet.Config{
+			DefaultHash:            crypto.SHA3_512,
+			DefaultCipher:          packet.CipherAES256,
+			DefaultCompressionAlgo: packet.CompressionZIP,
+			S2KCount:               65536 * 4, // Symmetric Key Generator expansion, should be > 65536
+		}
+	}
+	return openpgp.Encrypt(output, pubkeys, signer, filehint, config)
 }
